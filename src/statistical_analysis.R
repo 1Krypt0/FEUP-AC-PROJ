4a752f5572f9726d5575735eaf495e71a698ae6d
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2022-10-11 18:11:09 +0100

    Add barplot for amount loaned per district

--- src/statistical_analysis.R
+++ src/statistical_analysis.R
@@ -9,10 +9,28 @@ trans_data <- read.csv("data/trans_dev.csv", sep = ";")
 
 # Accounts per region
 account_district <- merge(account_data, district_data,
-    by.x = "district_id", by.y = "code"
+  by.x = "district_id", by.y = "code"
+)
+
+loans_per_district <- merge(account_district, loan_data,
+  by.x = "account_id", by.y = "account_id"
+)
+
+total_loans_per_district <- aggregate(
+  loans_per_districtit$amount,
+  list(loans_per_districtit$name),
+  FUN = sum
+)
+
+total_loans_per_district <- total_loans_per_district[
+  order(total_loans_per_district$x, decreasing = TRUE),
+]
+
+barplot(total_loans_per_district$x,
+  names.arg = total_loans_per_district$Group.1,
+  las = 2, cex.names = 0.6
 )
 
 barplot(sort(table(account_district$region),
-    decreasing = TRUE
+  decreasing = TRUE
 ), las = 2, cex.names = 0.6, ylim = c(0, 1000))
-
